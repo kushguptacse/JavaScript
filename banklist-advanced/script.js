c6963ccb01ed6a33835dd6100854b81323b4d499
CORPORATE_NT\kush.gupta <kush.gupta@kronos.com>
2021-05-31 23:45:14 +0530

    banklist advanced app navigation on li added - event bubbling

--- banklist-advanced/script.js
+++ banklist-advanced/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScroll = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function (event) {
   event.preventDefault();
@@ -19,6 +21,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
+const scrollEvent = () =>  section1.scrollIntoView({behavior:'smooth'});
+btnScroll.addEventListener('click',scrollEvent);
+
 btnsOpenModal.forEach(model=>model.addEventListener('click',openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
@@ -30,17 +35,28 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-//practice - add cookie
-const header = document.querySelector('.header');
-//create div element
-const msg = document.createElement('div');
-//add cookie-message class to div
-msg.classList.add('cookie-message');
-//add innerHTMl
-msg.innerHTML='We use Cookies. please accept <button class="btn btn--close--cookie">accept</button>';
+//attach click event parent link instead of assigning to each child link
+document.querySelector('.nav__links').addEventListener('click', event => {
+  event.preventDefault();
+  if(event.target.classList.contains('nav__link')) {
+    const id = event.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior:'smooth'});
+  }
+});
 
-//add message element to the end of header.
-header.append(msg);
-
-//delete message div on click of accept
-document.querySelector('.btn--close--cookie').addEventListener('click',()=>msg.remove());
+(() => {
+  const header = document.querySelector('.header');
+  //create div element
+  const msg = document.createElement('div');
+  //add cookie-message class to div
+  msg.classList.add('cookie-message');
+  //add innerHTMl
+  msg.innerHTML='We use Cookies. please accept <button class="btn btn--close--cookie">accept</button>';
+  
+  //add message element to the end of header.
+  header.append(msg);
+  
+  //delete message div on click of accept
+  document.querySelector('.btn--close--cookie').addEventListener('click',()=>msg.remove());
+  })();
+  
